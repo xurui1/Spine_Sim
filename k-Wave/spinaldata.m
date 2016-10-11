0646bf13568e461ea45389ae306c4275a0e10f83
Rui Xu <xurui@mcmaster.ca>
2016-10-11 16:27:59 -0400

    Added angle tests

---
+++ k-Wave/spinaldata.m
@@ -0,0 +1,65 @@
+function [RMS_p,Max_p,RMS_pnorm,Max_pnorm] = spinaldata(Nx,Ny,pmax_data,prms_data,medium_img)
+
+Num_spine_pts = 0;
+RMS_p = 0;
+Max_p = 0;
+RMS_pnorm = 0;
+Max_pnorm = 0;
+
+absolute_max_p = max(pmax_data(:));
+absolute_max_prms = max(prms_data(:));
+
+for i = 1:Nx
+    for j = 1:Ny
+        if medium_img(i,j) ==0
+           Iplus = 0;
+           i_local = i;
+           while i_local <= Nx &&  Iplus ==0
+               if medium_img(i_local,j) ==1;
+                   Iplus = 1;
+               end
+               i_local = i_local + 1;
+           end
+           
+           Iminus = 0;
+           i_local = i;
+           while i_local > 0 &&Iminus ==0
+                if medium_img(i_local,j) ==1;
+                    Iminus = 1;
+                end
+                i_local = i_local - 1;
+           end
+           
+           Jplus = 0;
+           j_local = j;
+           while j_local <= Ny &&  Jplus ==0
+               if medium_img(i,j_local) ==1;
+                   Jplus = 1;
+               end
+               j_local = j_local + 1;
+           end
+           
+           Jminus = 0;
+           j_local = j;
+           while j_local > 0 &&Jminus ==0
+                if medium_img(i,j_local) ==1;
+                    Jminus = 1;
+                end
+                j_local = j_local - 1;
+           end
+           
+           if Iplus+Iminus+Jplus+Jminus ==4
+               RMS_p = RMS_p + prms_data(i,j);
+               Max_p = Max_p + pmax_data(i,j);
+               Num_spine_pts = Num_spine_pts+1;
+           end
+           
+        end
+        
+    end
+end
+RMS_p = RMS_p / Num_spine_pts;
+Max_p = Max_p / Num_spine_pts;
+
+RMS_pnorm = RMS_p / (absolute_max_prms);
+Max_pnorm = Max_p / (absolute_max_p);
